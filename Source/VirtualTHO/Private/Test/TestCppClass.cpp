f688de6f00daa7716e1923748444608baeef4b04
Zake-arias <67579820+Zake-arias@users.noreply.github.com>
2021-11-29 23:27:33 +0800

    进行了一个转换测试
    如果此提交新增的内容导致无法用预编译版的UE4打开
    请及时反馈

---
+++ Source/VirtualTHO/Private/Test/TestCppClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test/TestCppClass.h"
+
+TestCppClass::TestCppClass()
+{
+}
+
+TestCppClass::~TestCppClass()
+{
+}
